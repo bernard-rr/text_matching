d41aba0a383bb534b6406b926b87629af70cb8c3
Chidi-Bernard Emmanuel <bernardchidi5@gmail.com>
2023-09-25 14:40:42 +0000

    update download button

--- app.py
+++ app.py
@@ -1,10 +1,6 @@
 import streamlit as st
-from helpers import process_uploaded_files, match_names
-
-def display_results(matched_data):
-    st.write("Matching results:")
-    st.table(matched_data)
-
+from helpers import process_uploaded_files, match_names, display_results, prepare_download
+import pandas as pd
 
 def main():
     st.title("Name Matching App")
@@ -16,6 +12,13 @@ def main():
         file1_data, file2_data = process_uploaded_files(uploaded_file1, uploaded_file2)
         matched_data = match_names(file1_data, file2_data)
         display_results(matched_data)
+        
+        # Add a download button for the matching results
+        download_button = st.download_button(
+            label="Download Matching Results (CSV)",
+            data=prepare_download(matched_data),
+            key="download_button"
+        )
 
 if __name__ == "__main__":
     main()
